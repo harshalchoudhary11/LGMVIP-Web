9403e579a53ab637aaf69e6e52f4b467d1aaa060
Harshal Choudhary <111169576+harshalchoudhary11@users.noreply.github.com>
2023-01-21 17:15:06 +0530

    commit

--- Task-3/Javascript.js
+++ Task-3/Javascript.js
@@ -1,7 +1,7 @@
-var Done = document.getElementById('Add-Btn')
-var Table= document.getElementById('display')
+const Done = document.getElementById('Add-Btn')
+const Table= document.getElementById('display')
 
-var Row = 1
+let Row = 1
 Done.onclick = function () {
 
     const Name = document.getElementById('name').value
@@ -11,19 +11,25 @@ Done.onclick = function () {
     const Image = document.createElement('img')
 
     if (Name != '' && Email != '' && Age != '' && Address != '') {
-        var values = []
-        var Selected_Value = document.querySelectorAll('input[type="checkbox"]:checked')
+        let values = []
+        let Selected_Value = document.querySelectorAll('input[type="checkbox"]:checked')
         Array.from(Selected_Value).forEach(function (e) {
             values.push(e.value)
         })
 
-        const description = `<p><strong>Name:</strong> ${Name}<br><strong>Email:</strong> ${Email}<br><strong>Age:</strong> ${Age}<br><strong>Address: </strong> ${Address}<br><strong>Gender:</strong> ${document.querySelector('input[type="radio"]:checked').value}
+        const description =
+         `<p><strong>Name:</strong> ${Name}
+         <br><strong>Email:</strong> ${Email}
+         <br><strong>Age:</strong> ${Age}
+         <br><strong>Address: </strong> ${Address}
+         <br><strong>Gender:</strong> 
+         ${document.querySelector('input[type="radio"]:checked').value}
         <br><strong>Skills:</strong> ${values.join(', ')}</p>
         `
-        var NewRow = Table.insertRow(Row)
+        let NewRow = Table.insertRow(Row)
 
-        var Cell_1 = NewRow.insertCell(0)
-        var Cell_2 = NewRow.insertCell(1)
+        let Block_1 = NewRow.insertCell(0)
+        let Block_2 = NewRow.insertCell(1)
 
         if (document.querySelector('input[type="radio"]:checked').value == 'Male') {
             Image.setAttribute('src', 'Image/Boy.jpg')
@@ -38,8 +44,8 @@ Done.onclick = function () {
             Image.setAttribute('alt', 'Others')
         }
 
-        Cell_1.innerHTML = description
-        Cell_2.appendChild(Image)
+        Block_1.innerHTML = description
+        Block_2.appendChild(Image)
 
         Row++
         document.getElementById('input-form').reset()
